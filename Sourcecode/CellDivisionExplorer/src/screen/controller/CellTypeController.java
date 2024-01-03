577fae8599721e925d6bf1629469e42a7012c8d3
DuongDH03 <duong.dh215020@sis.hust.edu.vn>
2024-01-04 00:57:22 +0700

    Added Back button

--- Sourcecode/CellDivisionExplorer/src/screen/controller/CellTypeController.java
+++ Sourcecode/CellDivisionExplorer/src/screen/controller/CellTypeController.java
@@ -1,10 +1,16 @@
 package screen.controller;
 
+import javafx.scene.Parent;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import javafx.event.*;
+
+import java.io.IOException;
 
 import cell.Cell;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 public class CellTypeController {
 	private Cell cell;
@@ -17,6 +23,9 @@ public class CellTypeController {
     private Button btnPro;
 
     @FXML
+    private Button btnBack;
+
+    @FXML
     void btnEuPressed(ActionEvent event) {
     	
     }
@@ -24,6 +33,22 @@ public class CellTypeController {
     @FXML
     void btnProPressed(ActionEvent event) {
 
+    }
+
+    @FXML
+    void btnBackPressed(ActionEvent event) {
+    	try {
+            final String MAIN_MENU_PATH = "/screen/view/MainMenu.fxml";
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(MAIN_MENU_PATH));
+            fxmlLoader.setController(new MainMenuController(cell));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Cell Division Explorer");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public CellTypeController(Cell cell, String menuChoice) {
